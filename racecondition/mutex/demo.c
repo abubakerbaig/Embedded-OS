4c27963fda2cc8ce2ac4a0e7ca81aa40f83507ff
Mirza Abubaker <abbaker.mirza11@gmail.com>
2020-10-31 02:35:07 -0700

    Mutex for INC and DEC threads

---
+++ racecondition/mutex/demo.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<pthread.h>
+
+pthread_mutex_t count_mutex;
+pthread_mutexattr_t count_mutex_attr;
+int count;
+
+void *inc_thread(void *argc)    {
+    while(1)    {
+        pthread_mutex_lock(&count_mutex);
+        count ++;
+        printf("inc_thread: %d \n",count);
+        pthread_mutex_unlock(&count_mutex);
+    }
+}
+
+void *dec_thread(void *argc)    {
+    while(1)    {
+        pthread_mutex_lock(&count_mutex);
+        count --;
+        printf("dec_thread: %d \n",count);
+        pthread_mutex_unlock(&count_mutex);
+    }
+}
+
+int main(int argc, char const argv[])   {
+
+    pthread_t inc_tid, dec_tid;
+
+    pthread_mutexattr_init(&count_mutex_attr);
+
+    pthread_mutex_init( &count_mutex, &count_mutex_attr);
+
+    pthread_create(&inc_tid, NULL, inc_thread, NULL);
+    pthread_create(&dec_tid, NULL, dec_thread, NULL);
+
+    pthread_join(inc_tid, NULL);
+    pthread_join(dec_tid, NULL);
+
+    pthread_mutex_destroy(&count_mutex);   
+    pthread_mutexattr_destroy(&count_mutex_attr);
+    return 0;
+
+}
