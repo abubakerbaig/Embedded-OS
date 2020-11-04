57e3e7229c985909bf43657b553cdcad86ee98f6
Mirza Abubaker <abbaker.mirza11@gmail.com>
2020-11-03 23:29:25 -0800

    Signaling Semaphor for controling execution

--- racecondition/signaling_semaphor/demo.c
+++ racecondition/signaling_semaphor/demo.c
@@ -2,7 +2,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t semaphore;
+sem_t semaphore1,semaphore2;
 int a=0, b=0;
 int sum;
 
@@ -10,22 +10,23 @@ void *input_thread(void *argc)  {
 
     printf("in input thread\n");
     
-    sem_wait(&semaphore);
-
+    sem_wait(&semaphore1);
+    
     printf("Enter a and b values: \n");
     scanf("%d %d",&a, &b);
     
-    sem_post(&semaphore);
+    
+    sem_post(&semaphore2);
     
 }
 
 void *exe_thread( void *agrc )  {
     
-    sem_wait(&semaphore);
+    sem_wait(&semaphore2);
     printf("in execution thread\n");
     sum=a+b;
     //printf("sum= %d\n",sum);
-    sem_post(&semaphore);
+    sem_post(&semaphore1);
     return (int*)(&sum);
     
 
@@ -35,7 +36,8 @@ int main (int argc, char const *argv[] )    {
     pthread_t ip,ex;
     void *sum1;
     
-    sem_init(&semaphore, 0, 1);
+    sem_init(&semaphore1, 0, 1);
+    sem_init(&semaphore2, 0, 0);
 
     pthread_create(&ip, NULL, input_thread, NULL);
     pthread_create(&ex, NULL, exe_thread, NULL);
