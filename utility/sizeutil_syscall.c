f3b15654c93b6abbab502dbf4491a67a88f2aaa4
Mirza Abubaker <abbaker.mirza11@gmail.com>
2020-11-14 10:58:34 -0800

    size utility using systams call

---
+++ utility/sizeutil_syscall.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char const *agrv[])  {
+
+    struct stat buf;
+
+    stat(agrv[1], &buf);
+
+    printf("Zide of the file \"%s\" is %lld bytes.\n", agrv[1], buf.st_size);
+
+    return 0;
+}
