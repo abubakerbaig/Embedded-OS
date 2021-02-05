36f5d92945ea288c8893aac80d467d37e12a8cd9
Mirza Abubaker <abbaker.mirza11@gmail.com>
2021-02-05 03:08:02 -0800

    shared memory protected with named semaphore

--- SharedMemory/consumer.c
+++ SharedMemory/consumer.c
@@ -1,40 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <sys/stat.h>        /* For mode constants */
-#include <fcntl.h>           /* For O_* constants */
+#include <sys/sem.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <semaphore.h>
 
-//#define SNAME1 "/mysem1"
-//#define SNAME2 "/mysem2"
+#define SNAME1 "/mysem1"
+#define SNAME2 "/mysem2"
 
 char *data;
 
-int main(int argc, char const *argv[])  {
+int main(int argc, char const *argv[])
+{
+    //creating semaphore 
+    sem_t *sem_prod = sem_open(SNAME1, O_CREAT, 0600, 0);
+    if(sem_prod == SEM_FAILED)  {
+        perror("sen_open/producer");
+        exit(EXIT_FAILURE);
+    }
 
-   // sem_t *sem_id1 = sem_open(SNAME1, 1);
-   // sem_t *sem_id2 = sem_open(SNAME2, 0);
+    sem_t *sem_cons = sem_open(SNAME2,O_CREAT, 0600, 1);
+
+    if(sem_cons == SEM_FAILED)  {
+        perror("sen_open/consumer");
+        exit(EXIT_FAILURE);
+    }
 
     int shmfd = shm_open("/CDACshmry", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    
-    ftruncate(shmfd, 10*sizeof(char)); 
-    
-    data = (char *) mmap(NULL, 10*sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
 
-    while(1)    {
-//        sem_wait(SNAME2);
-        printf("string received from shared memory is : %s\n", data);  
-        sleep(1);  
-//        sem_post(SNAME1);
+    ftruncate(shmfd, 10 * sizeof(char));
+
+    data = (char *)mmap(NULL, 10 * sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+
+    while (1)
+    {
+        printf("Consumer: waiting for lock\n");
+        sem_wait(sem_prod); //wait for producer to produce something
+        printf("Consumer: Accquiring lock \n Consumer: string received from shared memory is : %s\n", data);
+        sleep(1);
+        printf("Consumer: Realising lock\n");
+        sem_post(sem_cons); //signal producer data read
+        printf("Consumer: Relised lock\n");
     }
- //   sem_close(SNAME1);
-//    sem_close(SNAME2);
+    //   sem_close(SNAME1);
+    //    sem_close(SNAME2);
 
     shm_unlink("/CDACshmry");
 
-       return 0;
-
+    return 0;
 }
--- SharedMemory/procucer.c
+++ SharedMemory/procucer.c
@@ -1,47 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include <sys/stat.h>        /* For mode constants */
-#include <fcntl.h>           /* For O_* constants */
+#include <sys/sem.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <semaphore.h>
 #include <string.h>
 
-//#define SNAME1 "/mysem1"
-//#define SNAME2 "/mysem2"
+#define SNAME1 "/mysem1"
+#define SNAME2 "/mysem2"
 
 char *str;
 char data[10];
 
-int main(int argc, char const *argv[])  {
-
-    //sem_t *sem_id1 = sem_open(SNAME1, O_CREAT, 0644, 1);
-    //sem_t *sem_id2 = sem_open(SNAME2, O_CREAT, 0644, 0);
-    
-    int shmfd = shm_open("/CDACshmry", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    
-    ftruncate(shmfd, 10*sizeof(char)); 
-    
-    str= (char *) mmap(NULL, 10*sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
-    printf("mmap return - %x\n", str);
-
-    while(1)    {
-
-      //sem_wait(SNAME1);
-        printf("enter string to store in shared memory:\n");
-        scanf("%s",data);
-        strcpy(str, data);
-        printf("mmap return - %x", str);
-      //sem_post(SNAME2);
-        //printf("%s\n",str);
-
+int main(int argc, char const *argv[])
+{
+//opening semaphore
+  sem_t *sem_prod = sem_open(SNAME1, 0);
+    if(sem_prod == SEM_FAILED)  {
+        perror("sen_open/producer");
+        exit(EXIT_FAILURE);
     }
-   // sem_close(SNAME1);
-   // sem_close(SNAME2);
-    shm_unlink("/CDACshmry");
 
-    return 0;
+  sem_t *sem_cons = sem_open(SNAME2, 1);
 
+    if(sem_cons == SEM_FAILED)  {
+        perror("sen_open/consumer");
+        exit(EXIT_FAILURE);
+    }
+
+  int shmfd = shm_open("/CDACshmry", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+  ftruncate(shmfd, 10 * sizeof(char));
+
+  str = (char *)mmap(NULL, 10 * sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+  printf("producer: mmap return - %x\n", str);
+
+  while (1)
+  {
+    printf("producer: checking for lock\n");
+    sem_wait(sem_cons); //wait for consumer to have an open slot
+    printf("producer: Inside producer, accquired lock\nproducer: enter string to store in shared memory:\n");
+    scanf("%s", data);
+    strcpy(str, data);
+    //printf("producer: mmap return - %x", str);
+    printf("producer: Realising lock");
+
+    sem_post(sem_prod); //signal that something is being produced
+    printf("producer: realising Lock\n");
+  }
+   sem_close(sem_prod);
+   sem_close(sem_cons);
+  shm_unlink("/CDACshmry");
+
+  return 0;
 }
