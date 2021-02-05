b80720f5f2917a29be863bc3cbb1927004f99874
Mirza Abubaker <abbaker.mirza11@gmail.com>
2021-02-05 06:00:33 -0800

    protecting shared memory using single named semaphor which requires sleep operation to allow other process get sem or else only one process gets the semaphore other doesnt

---
+++ SharedMemory/with1sem/consumer.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/sem.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+
+#define SNAME1 "/mysem1"
+
+
+char *data;
+
+int main(int argc, char const *argv[])
+{
+    //creating semaphore 
+      sem_unlink(SNAME1);
+   
+    sem_t *sem_prod = sem_open(SNAME1, O_CREAT, 0600, 1);
+    if(sem_prod == SEM_FAILED)  {
+        perror("sen_open/producer");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    int shmfd = shm_open("/CDACshmry", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+    ftruncate(shmfd, 10 * sizeof(char));
+
+    data = (char *)mmap(NULL, 10 * sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+
+    while (1)
+    {
+        printf("Consumer: waiting for lock\n");
+        sleep(1);
+        sem_wait(sem_prod); //since sem= 0 ; wait for producer to produce something
+        printf("Consumer: Accquiring lock \n Consumer: string received from shared memory is : %s\n", data);
+        sleep(1);
+        printf("Consumer: Realising lock\n");
+        sem_post(sem_prod); //signal producer data read
+        printf("Consumer: Relised lock\n");
+    }
+       sem_close(sem_prod);
+
+
+    shm_unlink("/CDACshmry");
+    printf("Exiting");
+    return 0;
+}
---
+++ SharedMemory/with1sem/producer.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/sem.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <string.h>
+
+#define SNAME1 "/mysem1"
+
+
+char *str;
+char data[10];
+
+int main(int argc, char const *argv[])
+{
+//opening semaphore
+
+  sem_t *sem_prod = sem_open(SNAME1, 1);
+    if(sem_prod == SEM_FAILED)  {
+        perror("sen_open/producer");
+        exit(EXIT_FAILURE);
+    }
+
+
+  int shmfd = shm_open("/CDACshmry", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+  ftruncate(shmfd, 10 * sizeof(char));
+
+  str = (char *)mmap(NULL, 10 * sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+
+  while (1)
+  {
+    printf("producer: checking for lock\n");
+    sleep(1);
+    sem_wait(sem_prod); //since sem_cons= 1; wait for consumer to have an open slot
+    printf("producer: Inside producer, accquired lock\nproducer: enter string to store in shared memory:\n");
+    scanf("%s", data);
+    strcpy(str, data);
+    printf("producer: Realising lock");
+    sem_post(sem_prod); //signal that something is being produced
+    printf("producer: realising Lock\n");
+  }
+
+  sem_close(sem_prod);
+  shm_unlink("/CDACshmry");
+  printf("exiting");
+  return 0;
+}
